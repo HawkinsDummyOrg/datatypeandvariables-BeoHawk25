08ffa4f3a4f80983dd50d76c902e04868e28db18
Hawkins <83984558+BeoHawk25@users.noreply.github.com>
2022-10-21 18:12:12 -0400

    Update Program.cs

--- DataTypeAndVariables/Program.cs
+++ DataTypeAndVariables/Program.cs
@@ -8,18 +8,7 @@ namespace DataTypeAndVariables
       {
           Console.WriteLine("Hello World!");
 
-          byte myByte;
-          sbyte mySbyte;
-          int myInt;
-          uint myUint;
-          short myShort;
-          ushort myUShort;
-          float myFloat;
-          double myDouble;
-          char myCharacter;
-          bool myBool;
-          string myText;
-          string numText;
+
       }
 
       public static int Text2Num(string numText)
